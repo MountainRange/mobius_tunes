a41755f84f670521ac4eb27830433bbf710418b9
Josh Ting <joshting25@gmail.com>
2015-09-25 23:28:04 -0400

    Create playlist of songs

--- mobius.py
+++ mobius.py
@@ -5,21 +5,33 @@ import audioop
 import random
 from pydub import AudioSegment
 from pydub.playback import play
+from operator import itemgetter
 
 # returns a list of lists of points in the song with similar frequencies
-def loadConnections(song):
+def loadConnections(playList[0]):
 	return []
+
+def fractureSong(song, connections):
+	fragments = []
+	for breakPoint in connections:
+		fragments.append(song[:])
+	return fragments
 
 def main():
 	# initialize variables
-	song = AudioSegment.from_mp3("testmusic/funkychunk.mp3")
+	playlist = []
+	song = AudioSegment.from_mp3("testmusic/funkychunk.mp3") #for each song
+	playList.append(song)
 
 	# load connections list with song links
-	connections = loadConnections(song)
+	connections = loadConnections(playList)
+	sorted(connections, key = itemgetter(0))
 
 	# play song. We need to allow the song to randomly jump via connections[]
-	print("hi")
+	songFragments = fractureSong(song, connections)
+	print(connections)
+	play(song)
 	random.randint(0,2)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
