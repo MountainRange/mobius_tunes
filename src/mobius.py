4cc86c8a9273bbf5d1d201dda1c595714490df6a
rmkeezer <rmkeezer@yahoo.com>
2015-09-27 00:53:53 -0400

    fixed everything for the last time

--- src/mobius.py
+++ src/mobius.py
@@ -101,6 +101,7 @@ class mobius_py:
 				i -= 499
 			if i in fragDict:
 				if random.randint(0, 4) == 1:
+					print (len(currentfrags))
 					self.fileloader.play_raw_data(wavedata, currentfrags, queue = True)
 					time.sleep(10)
 					currentfrags = b''
