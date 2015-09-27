27ec9b8ed63df351dd63290ba556ab4432bc4ccd
Jay Kamat <github@jgkamat.33mail.com>
2015-09-27 01:25:23 -0400

    Make progress bars look way nicer

--- src/progress_bar.py
+++ src/progress_bar.py
@@ -7,6 +7,7 @@ class progress_bar:
 
 	def __init__(self, sizein):
 		self.size = sizein
+		self.last = 0
 
 		self.spinny = deque(['|', '/', '-', '\\' ])
 
@@ -14,26 +15,42 @@ class progress_bar:
 		return self.size
 
 	def update_bar(self, length):
-		print(self.spinny[0] + " ", end = "")
+		length = int(length)
+		print("[" + self.spinny[0] + "] ", end = "")
 		tmp = self.spinny.popleft()
 		self.spinny.append(tmp)
 
+		if self.last > length:
+			print("", end = "\n")
+			raise ValueError('Your new bar value decreased!')
+
 		if length > self.size:
+			print("", end = "\n")
 			raise ValueError('You entered a len > size for the bar')
 
-		for j in range(length):
-			print('#', end = "")
+		print('[', end = "")
+		for j in range(length - 1):
+			print('=', end = "")
+		print('>', end = "")
+
+
+		# Edge case for start
+		if length == 0:
+			length += 1
 		for j in range(self.size - length - 1):
-			print('_', end = "")
+			print('-', end = "")
+		print(']', end = "")
 
 		time.sleep(0.1)
 		print("", end = "\r")
 
-		if length >= self.size - 1:
-			print("", end = "\n")
+	def complete(self):
+		self.update_bar(self.size)
+		print("", end = "\n")
+		last = self.size
 
 if __name__ == "__main__":
-	bar = progress_bar(90)
-	for i in range(len(bar)):
-		bar.update_bar(i)
-		time.sleep(0.1)
+	bar = progress_bar(20)
+	for i in range(len(bar) * 10):
+		bar.update_bar(int(i / 10.0))
+	bar.complete()
